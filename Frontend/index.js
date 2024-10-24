8c110ee4ac7b46870f4cd1f22feff2e1ed0659d6
DawlatHamad <100542512+DawlatHamad@users.noreply.github.com>
2024-10-24 18:14:06 -0400

    Update index.js

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,6 +1,6 @@
 const signUpButton = document.getElementById('signUpButton');
 const signInButton = document.getElementById('signInButton');
-const signInForm = document.getElementById('signin'); // Adjusted to use the correct ID
+const signInForm = document.getElementById('signin'); 
 const signUpForm = document.getElementById('signup');
 
 signUpButton.addEventListener('click', function() {
